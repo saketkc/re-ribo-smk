63e84d524557e4dfc3264932eb66ebd5e9966a10
Saket Choudhary <saketkc@gmail.com>
2018-09-25 14:07:53 -0700

    Add wrapper

--- snakemake/wrappers/merge_fastq_wrapper.py
+++ snakemake/wrappers/merge_fastq_wrapper.py
@@ -1,11 +1,4 @@
 import os
 from snakemake.shell import shell
 
-if len(snakemake.input.dynamic_input) > 1:
-    shell(r'''
-          cat {snakemake.input.dynamic_input} > {snakemake.output}
-          ''')
-else:
-    source = os.path.abspath(str(snakemake.input.dynamic_input[0]))
-    destination = os.path.abspath(str(snakemake.output))
-    shell(r'''cp {source} {destination}''')
+shell(r'''cat {snakemake.input.dynamic_input} > {snakemake.output}''')
---
+++ snakemake/wrappers/trimming_wrapper.py
@@ -0,0 +1,37 @@
+from snakemake.shell import shell
+from riboraptor.kmer import fastq_kmer_histogram
+import operator
+
+def get_top_kmer(kmer_series_dict):
+    """Return a kmer if it's percentage exceed 30%"""
+    # Start from the longest kmer and stop
+    # at where this criterion is met
+    for kmer_length, kmer_series in sorted(kmer_series_dict.items(), key=operator.itemgetter(0), reverse=True):
+        over_represented = kmer_series[kmer_series>=30]
+        if len(over_represented)>=1:
+            return over_represented.index.tolist()[0]
+    return None
+
+
+params = snakemake.params
+pass1_dir = snakemake.params.pass1_dir
+pass2_dir = snakemake.params.pass2_dir
+output_1 = snakemake.output.pass1_fq
+output_2 = snakemake.output.pass2_fq
+
+# Do first pass
+shell(r'''trim_galore -o {pass1_dir} --length {params.min_length} \
+      -q {params.phred_cutoff} {snakemake.input.R1}''')
+
+
+# Are there any  over-represented sequences?
+# If yes, do a second pass
+histogram = fastq_kmer_histogram(output_1)
+adapter = get_top_kmer(histogram)
+if adapter is None:
+    shell(r'''cp -r {output_1} {output_2}''')
+else:
+    # Else just copy
+    shell(r'''trim_galore -o {pass2_dir} --length {params.min_length} \
+        -a {adapter} \
+        -q {params.phred_cutoff} {output_1}''')

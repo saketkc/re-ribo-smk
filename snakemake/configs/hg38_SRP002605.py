8e04a329fa8089931b02298502b1bc8191e52c1f
Saket Choudhary <saketkc@gmail.com>
2018-09-30 18:16:56 -0700

    Add configs

---
+++ snakemake/configs/hg38_SRP002605.py
@@ -0,0 +1,17 @@
+
+RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/hg38/SRP002605'
+OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/hg38/SRP002605'
+GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.fa'
+CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.chrom.sizes'
+STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/hg38/star_annotated'
+GTF = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.annotation.gtf'
+GENE_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/gene.bed.gz'
+STAR_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/start_codon.bed.gz'
+STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/stop_codon.bed.gz'
+CDS_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/cds.bed.gz'
+UTR5_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/utr5.bed.gz'
+UTR3_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/utr3.bed.gz'
+INTRON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/hg38/v25/intron.bed.gz'
+ORIENTATIONS = ['5prime', '3prime']
+STRANDS = ['pos', 'neg', 'combined']
+FRAGMENT_LENGTHS =  range(18, 39)

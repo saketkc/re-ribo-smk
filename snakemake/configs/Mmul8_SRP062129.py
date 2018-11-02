65143382130f130024b106b363a5ee1e8341a91c
Saket Choudhary <saketkc@gmail.com>
2018-11-01 18:51:18 -0700

    Add data

---
+++ snakemake/configs/Mmul8_SRP062129.py
@@ -0,0 +1,17 @@
+
+RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/Mmul8/SRP062129'
+OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/Mmul8/SRP062129'
+GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/Mmul8/fasta/Macaca_mulatta.Mmul_8.0.1.dna.toplevel.fa'
+CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/Mmul8/fasta/Macaca_mulatta.Mmul_8.0.1.dna.toplevel.sizes'
+STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/Mmul8/star_annotated'
+GTF = '/home/cmb-panasas2/skchoudh/genomes/Mmul8/annotation/Macaca_mulatta.Mmul_8.0.1.94.gtf'
+GENE_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/gene.bed.gz'
+STAR_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/start_codon.bed.gz'
+STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/stop_codon.bed.gz'
+CDS_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/cds.bed.gz'
+UTR5_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/utr5.bed.gz'
+UTR3_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/utr3.bed.gz'
+INTRON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/Mmul8/v94/intron.bed.gz'
+ORIENTATIONS = ['5prime', '3prime']
+STRANDS = ['pos', 'neg', 'combined']
+FRAGMENT_LENGTHS =  range(18, 39)
---
+++ snakemake/configs/panTro3_SRP062129.py
@@ -0,0 +1,17 @@
+
+RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/panTro3/SRP062129'
+OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/panTro3/SRP062129'
+GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/panTro3/fasta/Pan_troglodytes.Pan_tro_3.0.dna.toplevel.fa'
+CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/panTro3/fasta/Pan_troglodytes.Pan_tro_3.0.dna.toplevel.sizes'
+STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/panTro3/star_annotated'
+GTF = '/home/cmb-panasas2/skchoudh/genomes/panTro3/annotation/Pan_troglodytes.Pan_tro_3.0.94.gtf'
+GENE_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/gene.bed.gz'
+STAR_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/start_codon.bed.gz'
+STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/stop_codon.bed.gz'
+CDS_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/cds.bed.gz'
+UTR5_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/utr5.bed.gz'
+UTR3_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/utr3.bed.gz'
+INTRON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/panTro3/v94/intron.bed.gz'
+ORIENTATIONS = ['5prime', '3prime']
+STRANDS = ['pos', 'neg', 'combined']
+FRAGMENT_LENGTHS =  range(18, 39)

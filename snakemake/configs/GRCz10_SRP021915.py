c22b4e0f52f739c57a42c7e816966a1ac2b4ff77
Saket Choudhary <saketkc@gmail.com>
2018-11-09 08:25:33 -0800

    Add configs

---
+++ snakemake/configs/GRCz10_SRP021915.py
@@ -0,0 +1,17 @@
+
+RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/GRCz10/SRP021915'
+OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/GRCz10/SRP021915'
+GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/GRCz10/fasta/Danio_rerio.GRCz10.dna.toplevel.fa'
+CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/GRCz10/fasta/Danio_rerio.GRCz10.dna.toplevel.sizes'
+STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/GRCz10/star_annotated'
+GTF = '/home/cmb-panasas2/skchoudh/genomes/GRCz10/annotation/Danio_rerio.GRCz10.91.gtf'
+GENE_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/gene.bed.gz'
+STAR_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/start_codon.bed.gz'
+STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/stop_codon.bed.gz'
+CDS_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/cds.bed.gz'
+UTR5_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/utr5.bed.gz'
+UTR3_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/utr3.bed.gz'
+INTRON_BED = '/home/cmb-panasas2/skchoudh/github_projects/riboraptor/riboraptor/annotation/GRCz10/v91/intron.bed.gz'
+ORIENTATIONS = ['5prime', '3prime']
+STRANDS = ['pos', 'neg', 'combined']
+FRAGMENT_LENGTHS =  range(18, 39)

1de5e6b96f1b31b1720d45e97395dfc78d45ebce
Saket Choudhary <saketkc@gmail.com>
2019-06-25 12:35:51 -0700

    Add configs

---
+++ snakemake/kallisto-configs/homo_sapiens_SRP010679_single.py
@@ -0,0 +1,5 @@
+
+RAWDATA_DIR = '/staging/as/skchoudh/pysradb_downloads/SRP010679'
+OUT_DIR = '/staging/as/skchoudh/SRP010679_kallisto_cds_analysis'
+CDNA_IDX = '/home/cmb-panasas2/skchoudh/genomes/homo_sapiens/cdna/gencode.v25.transcripts.kallisto.index'
+CDS_IDX = '/home/cmb-panasas2/skchoudh/genomes/homo_sapiens/cds/gencode.v25.pc_transcripts.kallisto.index'

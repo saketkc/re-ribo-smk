902288101245042d5bcafd5c99761d7eadbad87d
Saket Choudhary <saketkc@gmail.com>
2020-01-22 12:33:58 -0800

    rename panasas

--- snakemake/configs/human_template.py
+++ snakemake/configs/human_template.py
@@ -5,37 +5,37 @@ RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/samples_to_process/'
 OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis'  #_alignEndToEnd'
 
 ## Genome fasta location
-GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.fa'
+GENOME_FASTA = '/home/qcb-panasas/skchoudh/genomes/hg38/fasta/hg38.fa'
 
 ## Chromosome sizes location
-CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/hg38/fasta/hg38.chrom.sizes'
+CHROM_SIZES = '/home/qcb-panasas/skchoudh/genomes/hg38/fasta/hg38.chrom.sizes'
 
 ## Path to STAR index (will be generated if does not exist)
-STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/hg38/star_annotated'
+STAR_INDEX = '/home/qcb-panasas/skchoudh/genomes/hg38/star_annotated'
 
 ## GTF path
-GTF = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.annotation.without_rRNA_tRNA.gtf'
+GTF = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.annotation.without_rRNA_tRNA.gtf'
 
 ## GenePred bed downloaded from UCSC
 ## (this is used for inferring the type of experiment i.e stranded/non-stranded
 ## and hence is not required)
-GENE_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v24.genes.bed'
+GENE_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v24.genes.bed'
 
 ## Path to bed file with start codon coordinates
-START_CODON_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.start_codon.bed'
+START_CODON_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.start_codon.bed'
 
 ## Path to bed file with stop codon coordinates
-STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.stop_codon.bed'
+STOP_CODON_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.stop_codon.bed'
 
 ## Path to bed file containing CDS coordinates
-CDS_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.cds.bed'
+CDS_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.cds.bed'
 
 # We don't have these so just use CDs bed to get the pipeline running
-UTR5_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.UTR5.bed'
+UTR5_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.UTR5.bed'
 
-UTR3_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.UTR3.bed'
+UTR3_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.UTR3.bed'
 
-INTRON_BED = '/home/cmb-panasas2/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.intron.bed'
+INTRON_BED = '/home/qcb-panasas/skchoudh/genomes/hg38/annotation/gencode.v25.gffutils.intron.bed'
 
 ORIENTATIONS = ['5prime', '3prime']
 
--- snakemake/configs/mm10/mouse_template.py
+++ snakemake/configs/mm10/mouse_template.py
@@ -5,37 +5,37 @@ RAWDATA_DIR = '/staging/as/skchoudh/re-ribo-datasets/samples_to_process/'
 OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/'
 
 ## Genome fasta location
-GENOME_FASTA = '/home/cmb-panasas2/skchoudh/genomes/mm10/fasta/mm10.fa'
+GENOME_FASTA = '/home/qcb-panasas/skchoudh/genomes/mm10/fasta/mm10.fa'
 
 ## Chromosome sizes location
-CHROM_SIZES = '/home/cmb-panasas2/skchoudh/genomes/mm10/fasta/mm10.chrom.sizes'
+CHROM_SIZES = '/home/qcb-panasas/skchoudh/genomes/mm10/fasta/mm10.chrom.sizes'
 
 ## Path to STAR index (will be generated if does not exist)
-STAR_INDEX = '/home/cmb-panasas2/skchoudh/genomes/mm10/star_annotated'
+STAR_INDEX = '/home/qcb-panasas/skchoudh/genomes/mm10/star_annotated'
 
 ## GTF path
-GTF = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.annotation.gtf'
+GTF = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.annotation.gtf'
 
 ## GenePred bed downloaded from UCSC
 ## (this is used for inferring the type of experiment i.e stranded/non-stranded
 ## and hence is not required)
-GENE_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.genes.bed'
+GENE_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.genes.bed'
 
 ## Path to bed file with start codon coordinates
-START_CODON_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.start_codon.bed'
+START_CODON_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.start_codon.bed'
 
 ## Path to bed file with stop codon coordinates
-STOP_CODON_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.stop_codon.bed'
+STOP_CODON_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.stop_codon.bed'
 
 ## Path to bed file containing CDS coordinates
-CDS_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.cds.bed'
+CDS_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.cds.bed'
 
 # We don't have these so just use CDs bed to get the pipeline running
-UTR5_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.UTR5.bed'
+UTR5_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.UTR5.bed'
 
-UTR3_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.UTR3.bed'
+UTR3_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.UTR3.bed'
 
-INTRON_BED = '/home/cmb-panasas2/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.intron.bed'
+INTRON_BED = '/home/qcb-panasas/skchoudh/genomes/mm10/annotation/gencode.vM11.gffutils.intron.bed'
 
 ORIENTATIONS = ['5prime', '3prime']
 

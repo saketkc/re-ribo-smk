c9326cbafdfa060e22e9af692d9146c37f5035ba
Saket Choudhary <saketkc@gmail.com>
2020-05-29 18:41:13 -0700

    BDGP6 special adapters

---
+++ snakemake/submitall.ribotricer.notemp.BDGP6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#conda activate ribopod
+snakemake --snakefile Snakefile.ribotricer.notemp.BDGP6_SRP131957\
+    --config config_path=configs/$1.py\
+    --js $PWD/jobscript.sh\
+    --printshellcmds\
+    --cluster-config $PWD/cluster.yaml\
+    --jobname "{rulename}.{jobid}.$1"\
+    --keep-going\
+    --stats $PWD/stats/$1.riboraptor.stats\
+    --rerun-incomplete\
+    -j 200\
+    --cluster 'sbatch --partition={cluster.partition} --nodes=1 --ntasks={cluster.cores} --mem={cluster.mem} --time={cluster.time} -o {cluster.logout} -e {cluster.logerror}'
+    #--dryrun\
